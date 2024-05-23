18ab76c2fa161d083cb12584eac82437eb2eac2d
enjoroge <njrogeelizabeth53@gmail.com>
2024-05-23 22:36:24 +0300

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { username, city } from "../data/user";
 
 function Home() {
   return (
     <div id="home">
-      <h1>
-        {username} is a Web Developer from {city}
-      </h1>
+      <h1>Home</h1>
+      <p>Username: {username}</p>
+      <p>City: {city}</p>
     </div>
   );
 }
+
+export default Home;
